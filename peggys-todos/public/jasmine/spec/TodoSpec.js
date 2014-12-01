85feee432dbda816d5dcf5d0a0559ecfe9b46a18
peggyfox <fox.peggy@gmail.com>
2014-12-01 08:21:34 -0600

    removed commented tests

--- peggys-todos/public/jasmine/spec/TodoSpec.js
+++ peggys-todos/public/jasmine/spec/TodoSpec.js
@@ -25,38 +25,6 @@ describe("User Forms", function() {
 
 });
 
-describe("Sessions", function() {
-  beforeEach(function() {
-    jasmine.Ajax.install();
-    loginUserFromClient("fox@mail.com","password");
-    request = jasmine.Ajax.requests.mostRecent();
-
-    describe("loginUserFromClient", function() {
-      beforeEach(function() {
-      request.response(TestResponses.user);
-      
-      });
-      it("should start a user session if the user exists", function() {
-
-        expect(Todo.USER.id).toEqual(519);
-      });
-
-    })
-
-  }
-
-  // describe("createUserFromClient",function() {
-
-  //   it("should create a new user and start a session", function() {
-  //     // expect(createUserFromClient("email","password"))
-
-  //   })
-
-  // })
-
-
-})
-
 describe("Todos", function() {
 
   describe("createTodoForm", function() {
@@ -91,57 +59,4 @@ describe("Todos", function() {
   })
 
 })
-
-
-
-
-
-  // it("should be able to play a Song", function() {
-  //   player.play(song);
-  //   expect(player.currentlyPlayingSong).toEqual(song);
-
-  //   //demonstrates use of custom matcher
-  //   expect(player).toBePlaying(song);
-  // });
-
-  // describe("when song has been paused", function() {
-  //   beforeEach(function() {
-  //     player.play(song);
-  //     player.pause();
-  //   });
-
-  //   it("should indicate that the song is currently paused", function() {
-  //     expect(player.isPlaying).toBeFalsy();
-
-  //     // demonstrates use of 'not' with a custom matcher
-  //     expect(player).not.toBePlaying(song);
-  //   });
-
-  //   it("should be possible to resume", function() {
-  //     player.resume();
-  //     expect(player.isPlaying).toBeTruthy();
-  //     expect(player.currentlyPlayingSong).toEqual(song);
-  //   });
-  // });
-
-  // // demonstrates use of spies to intercept and test method calls
-  // it("tells the current song if the user has made it a favorite", function() {
-  //   spyOn(song, 'persistFavoriteStatus');
-
-  //   player.play(song);
-  //   player.makeFavorite();
-
-  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  // });
-
-  // //demonstrates use of expected exceptions
-  // describe("#resume", function() {
-  //   it("should throw an exception if song is already playing", function() {
-  //     player.play(song);
-
-  //     expect(function() {
-  //       player.resume();
-  //     }).toThrowError("song is already playing");
-  //   });
-  // });
 
